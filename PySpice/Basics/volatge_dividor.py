fae6a827c262934e41d979d808fdaa49dc67c3f1
Amin Hashemian <143293364+A-Hashemiann@users.noreply.github.com>
2024-03-21 22:01:16 +0100

    Update volatge_dividor.py

--- PySpice/Basics/volatge_dividor.py
+++ PySpice/Basics/volatge_dividor.py
@@ -1,1 +1,6 @@
 
+import matplotlib.pyplot as plt
+from PySpice.Doc.ExampleTools import plot_results
+from PySpice.Probe.Plot import plot
+from PySpice.Spice.Library import SpiceLibrary
+from PySpice.Spice.Netlist import Circuit
