af8282ede9aaa99e666aad8c9bb096fc40e46265
A-Hashemiann <amen.hashemian@gmail.com>
2024-05-25 00:38:37 +0200

    signal_driver

---
+++ Verilog/Signal_Driver/signal_driver.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+----------------------------------------
+
+entity Driver is
+port(	x: in std_logic;
+	F: out std_logic
+);
+end Driver;  
+
+----------------------------------------
