8632dc24b1d16103d6f3c81afc226994240ec0d4
A-Hashemiann <amen.hashemian@gmail.com>
2024-07-29 22:51:00 +0330

    inverter

---
+++ VHDL/Inverter/inverter.vhd
@@ -0,0 +1,9 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Inverter is
+    port (
+        A : in  std_logic;  -- Input signal
+        Y : out std_logic   -- Output signal
+    );
+end entity Inverter;
