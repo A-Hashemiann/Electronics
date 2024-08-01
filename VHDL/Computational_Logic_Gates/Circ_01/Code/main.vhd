4dd65ae38f6e21c43e539be31076a09029c87524
A-Hashemiann <amen.hashemian@gmail.com>
2024-08-01 23:45:31 +0330

    computational_logic_gate

---
+++ VHDL/Computational_Logic_Gates/Circ_01/Code/main.vhd
@@ -0,0 +1,12 @@
+
+library ieee;				-- top level circuit
+use ieee.std_logic_1164.all;
+use work.all;
+
+entity comb_ckt is
+port(	input1: in std_logic;
+	input2: in std_logic;
+	input3: in std_logic;
+	output: out std_logic
+);
+end comb_ckt;
