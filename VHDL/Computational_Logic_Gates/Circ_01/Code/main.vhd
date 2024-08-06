d9685fcfbc5e8aee84431873e62a426e5fcf0807
A-Hashemiann <amen.hashemian@gmail.com>
2024-08-06 22:30:57 +0330

    computational_logic_gates

--- VHDL/Computational_Logic_Gates/Circ_01/Code/main.vhd
+++ VHDL/Computational_Logic_Gates/Circ_01/Code/main.vhd
@@ -13,6 +13,11 @@ end comb_ckt;
 
 architecture struct of comb_ckt is
 
-  
+  component AND_GATE is		-- entity of AND_GATE
+    port(   A:	in std_logic;
+    	    B:	in std_logic;
+            F1:	out std_logic
+    );
+    end component;
 
 end struct;
