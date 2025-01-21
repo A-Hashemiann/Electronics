63d0f50210121bc56a3810ca1148f1376c44deef
A-Hashemiann <amen.hashemian@gmail.com>
2025-01-21 22:45:35 +0330

    quantum_state_encoder

--- Quantum_FPGA/Quantum_Logic_Modules/Quantum_State_Encoder/state_encodder.vhd
+++ Quantum_FPGA/Quantum_Logic_Modules/Quantum_State_Encoder/state_encodder.vhd
@@ -11,3 +11,26 @@ entity Quantum_State_Encoder is
         output_imag : out STD_LOGIC_VECTOR(7 downto 0)  -- Imaginary part of the encoded state
     );
 end Quantum_State_Encoder;
+
+begin
+    process(input_state)
+    begin
+        case input_state is
+            when "00" => -- Encode |0>
+                output_real <= STATE_ZERO(15 downto 8);
+                 output_imag <= STATE_ZERO(7 downto 0);
+            when "01" => -- Encode |1>
+                 output_real <= STATE_ONE(15 downto 8);
+                output_imag <= STATE_ONE(7 downto 0);
+            when "10" => -- Encode |+>
+                output_real <= STATE_PLUS(15 downto 8);
+                output_imag <= STATE_PLUS(7 downto 0);
+            when "11" => -- Encode |->
+                output_real <= STATE_MINUS(15 downto 8);
+                output_imag <= STATE_MINUS(7 downto 0);
+            when others =>
+                output_real <= (others => '0');
+                output_imag <= (others => '0');
+        end case;
+    end process;
+end Behavioral;
