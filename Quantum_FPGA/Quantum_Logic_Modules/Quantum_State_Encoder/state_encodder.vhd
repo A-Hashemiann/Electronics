2fe423f01a09af8d1e2224d201479e01615ced04
A-Hashemiann <amen.hashemian@gmail.com>
2025-01-21 22:40:18 +0330

    quantum_state_encoder

---
+++ Quantum_FPGA/Quantum_Logic_Modules/Quantum_State_Encoder/state_encodder.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Quantum State Encoder
+-- Encodes quantum states (|0>, |1>, |+>, |->) into binary outputs
+entity Quantum_State_Encoder is
+    Port (
+        input_state : in STD_LOGIC_VECTOR(1 downto 0); -- Quantum state selector
+        output_real : out STD_LOGIC_VECTOR(7 downto 0); -- Real part of the encoded state
+        output_imag : out STD_LOGIC_VECTOR(7 downto 0)  -- Imaginary part of the encoded state
+    );
+end Quantum_State_Encoder;
